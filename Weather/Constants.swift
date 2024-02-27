8d109f5c5b48cc19e284a10146b4fecbc398c914
Daniel Parra Martin <dparrama@emeal.nttdata.com>
2024-02-27 12:30:21 +0100

    Edit gitignore

---
+++ Weather/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Daniel Parra Martin on 27/2/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "79667bd718msh6b22b5455f95edbp1abb93jsnbf2b621c6229"
+}
